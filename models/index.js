70d91f11777ab8794f85fb4f3c894cdbd5d4e7c8
Tammam Khan <tammam.a.khan@gmail.com>
2022-05-10 19:50:19 -0700

    added associations between models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,28 @@ const User = require("./User");
 const Event = require("./Event");
 const GroupMember = require("./GroupMember");
 
+User.hasMany(Event, {
+  foreignKey: "user_id",
+});
+
+Event.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(GroupMember, {
+  foreignKey: "user_id",
+});
+
+GroupMember.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Event.hasMany(GroupMember, {
+  foreignKey: "event_id",
+});
+
+GroupMember.belongsTo(Event, {
+  foreignKey: "event_id",
+});
+
 module.exports = { User, Event, GroupMember };
