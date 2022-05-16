c7f7fdabc11701d1d33f4f5044a62a0cad7b330c
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-05-15 17:51:49 -0700

    Add files via upload

---
+++ controllers/home-routes.js
@@ -0,0 +1,61 @@
+const { GroupMember, User, Event } = require('../models');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) => {
+    Event.findAll({
+      attributes: [
+        'id',
+        'host_id',
+        'name',
+        'description',
+        'start',
+        'end',
+        'street_address',
+        'city',
+        'state',
+        'zip_code'
+      ],
+      include: [
+        {
+          model: GroupMember,
+          attributes: ['id', 'member_id', 'group_id'],
+          include: {
+            model: User,
+            attributes: ['email']
+          }
+        },
+        {
+          model: User,
+          attributes: ['email']
+        }
+      ]
+    })
+      .then(dbEventData => {
+        // pass a single post object into the homepage template
+        const events = dbEventData.map(event => event.get({ plain: true }));
+        res.render('home-page', { events, loggedIn: req.session.loggedIn });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+//Redirects user to login.
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return; 
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+router.get('/home-page', (req, res) => {
+  res.redirect('/');
+});
+
+
+module.exports = router; 
